06725357e79f1ee601606e9e3ea792912cb38e12
jazzari <passionjazz@gmail.com>
2020-09-08 14:33:18 +0200

    BDD to sign up with Devise

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -39,8 +39,48 @@ When /^I sign up with valid data$/ do
 	sign_up 
 end
 
+When /^I sign up with an invalid email$/ do 
+	create_visitor
+	@visitor = @visitor.merge(email: "invalidemail")
+	sign_up
+end
+
+When /^I sign up without a password$/ do 
+	create_visitor
+	@visitor = @visitor.merge(password: "")
+	sign_up
+end
+
+When /^I sign up without a password confirmation$/ do 
+	create_visitor
+	@visitor = @visitor.merge(password_confirmation: "")
+	sign_up
+end
+
+When /^I sign up with a mismatched password confirmation$/ do 
+	create_visitor
+	@visitor = @visitor.merge(password_confirmation: "notapassword")
+	sign_up
+end
 
 	## THEN ##
 Then /^I should see a successful sign up message$/ do 
 	expect(page).to have_content "Welcome! You have signed up successfully."
 end
+
+Then /^I should see an invalid email message$/ do 
+	expect(page).to have_content "Email is invalid"
+end
+
+Then /^I should see a missing password message$/ do 
+	expect(page).to have_content "Password can't be blank"
+end
+
+Then /^I should see a missing password confirmation message$/ do 
+	expect(page).to have_content "Password confirmation doesn't match Password"
+end
+
+Then /^I should see a mismatched password message$/ do 
+	expect(page).to have_content "Password confirmation doesn't match Password"
+end
+
