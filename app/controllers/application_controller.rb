b30bbe8c83aeb35c1f3ec80354b43306023b8368
jazzari <passionjazz@gmail.com>
2020-09-14 17:36:06 +0200

    Adding authentication to AdminPanel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: :index
   
-  
+  def authenticate_admin_user!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "Unauthorized Access!"
+      redirect_to root_path
+    end
+  end
 end
