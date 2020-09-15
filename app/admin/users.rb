d4dcbcbf289e9b9dbf998915a4c841d60812d577
jazzari <passionjazz@gmail.com>
2020-09-15 15:19:04 +0200

    Fixing columns and new button in ActiveAdmin users model

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
-
+  remove_filter :reset_password_token, :reset_password_sent_at, :confirmation_token, :confirmed_at, :confirmation_sent_at
+  actions :all, :except => [:new]
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -15,4 +16,15 @@ ActiveAdmin.register User do
   #   permitted
   # end
   
+   index do
+     selectable_column
+     column :id
+     column :name
+     column :email
+     column :admin
+     column :created_at
+     column :updated_at
+     actions       
+   end
+
 end
