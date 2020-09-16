38be6282f5680aacf71dbe3a2c3cc3f916610a79
jazzari <passionjazz@gmail.com>
2020-09-16 15:41:08 +0200

    Disable confirmation email in Devise

---
+++ db/migrate/20200916130805_remove_confirmation_email_from_users.rb
@@ -0,0 +1,7 @@
+class RemoveConfirmationEmailFromUsers < ActiveRecord::Migration[5.2]
+  def change
+  	remove_column :users, :confirmation_token 
+  	remove_column :users, :confirmed_at
+  	remove_column :users, :confirmation_sent_at
+  end
+end
