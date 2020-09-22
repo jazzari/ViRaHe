8bb7be570bb77545bd18c59eb3714454c6df3ef3
jazzari <passionjazz@gmail.com>
2020-09-22 15:14:47 +0200

    Refactor login process in Omniauth controller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,29 +1,29 @@
 class OmniauthController < Devise::OmniauthCallbacksController
 
 	def facebook
-		@user = User.create_from_provider_data(request.env['omniauth.auth'])
-		if @user.persisted?
-			sign_in_and_redirect @user
-			set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-		else
-			flash[:error] = "There was a problem in signing you in through Facebook. Please try signing in later"
-			redirect_to new_user_registration_url
-		end
+		@user = set_login("facebook")
 	end
 
 	def google_oauth2
-		@user = User.create_from_provider_data(request.env['omniauth.auth'])
-		if @user.persisted?
-			sign_in_and_redirect @user
-			set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-		else
-			flash[:error] = "There was a problem in signing you in through Google. Please try signing in later"
-			redirect_to new_user_registration_url
-		end
+		@user = set_login("google")
 	end
 
-	def failure
-		flash[:error] = "There was a problem signing you in. Please try signing in later."
+	# def failure
+	# 	flash[:error] = "There was a problem signing you in. Please try signing in later."
+	# 	redirect_to new_user_registration_url
+	# end
+end
+
+private
+
+def set_login(provider)
+	@user = User.create_from_provider_data(request.env['omniauth.auth'])
+	if @user.persisted?
+		sign_in_and_redirect @user
+		set_flash_message(:notice, :success, kind: "#{provider.capitalize}") if is_navigational_format?
+	else
+		message = "There was a problem in signing you in through #{provider.capitalize}. Please try signing in later"
+		flash[:error] = message
 		redirect_to new_user_registration_url
 	end
 end
