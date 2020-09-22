a531712666030bd4a0fbbabd868d8204d0652a31
jazzari <passionjazz@gmail.com>
2020-09-22 09:16:38 +0200

    Rspec of other omniauth providers

--- spec/controllers/omniauth_controller_spec.rb
+++ spec/controllers/omniauth_controller_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe OmniauthController do 
-	before do
-		request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
-  		request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook] 
+	describe "#FACEBOOK_AUTH" do 
+		before do
+			request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
+	  		request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook] 
+		end
+
+		it "should be valid" do 
+			expect(response).to be_successful
+		end
+
+		it "should reject invalid provider" do 
+			request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:some_provider]
+			expect(response).not_to have_http_status(403)
+		end
 	end
 
-	it "should be valid" do 
-		expect(response).to be_success
-	end
+	describe "#GOOGLE_AUTH" do 
+		before do
+			request.env["devise.mapping"] = Devise.mappings[:user] # If using Devise
+	  		request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google] 
+		end
 
+		it "should be valid" do 
+			expect(response).to be_successful
+		end
+	end
 end
