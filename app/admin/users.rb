ff3a3b60674165e2ea37a5c7f9f8735e4954ea53
jazzari <passionjazz@gmail.com>
2020-09-24 08:40:13 +0200

    Adding NewUser button to ActiveAdmin User model

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,20 +1,5 @@
 ActiveAdmin.register User do
   remove_filter :reset_password_token, :reset_password_sent_at, :confirmation_token, :confirmed_at, :confirmation_sent_at
-  actions :all, :except => [:new]
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :name, :admin
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :name, :admin]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
   
    index do
      selectable_column
